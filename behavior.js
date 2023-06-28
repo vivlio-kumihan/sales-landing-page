0966d07dcb889ab74fc394d5ea54c652d1badabd
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-06-29 05:19:31 +0900

    最初は透明から姿を現し、フッターの前でくっつく挙動

--- behavior.js
+++ behavior.js
@@ -1,12 +1,22 @@
 ScrollTrigger.create({
   trigger: ".box",
   // 見た目がわかりやすいので、まずobjectのtopとしておいて、スクリーンの高さの何%の位置かを決める。
-  start: "top 80%",
+  start: "top 75%",
   endTrigger: "footer",
   // ここでくっついて行く位置を決める。
   // footerのtopで、次の値は見ながら適宜決める。
   end: "top 90.5%",
   pin: true,
   pinSpacing: false,
-  markers: true
+  // markers: true
+})
+
+gsap.to('#box', .3, {
+  opacity: 1,
+  ease: 'power1.inOut',
+  scrollTrigger: {
+    trigger: '#trigger',
+    start: '0 50%',
+    markers: true
+  }
 })
